e763b9c1224b99de823794dad5f021e48ae46b27
RomanPas <unlike05@gmail.com>
2020-04-18 16:57:38 +0300

    fix main

--- main.cpp
+++ main.cpp
@@ -9,18 +9,14 @@ using namespace std;
 int main(int argc, char const* argv[]) {
 
 	vector<uint32_t> ips = readData();
-	cout << ips.size() << endl;
 	sort(ips.rbegin(), ips.rend());
 
 	for (auto ip : ips) {
 		printIp(ip);
 	}
 
-	cout << endl;
 	filter(ips, 1);
-	cout << endl;
 	filter(ips, 46, 70);
-	cout << endl;
 	filterAny(ips, 46);
 
 	return 0;
